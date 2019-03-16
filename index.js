44fb843465d1d4f10f8a2509e76f4843db9ae3dd
pdhoward <patrick.howard@hotmail.com>
2019-03-16 09:56:30 -0400

    updates

--- index.js
+++ index.js
@@ -28,3 +28,53 @@ function getData(auth) {
 authentication.authenticate().then((auth)=>{
     getData(auth);
 });
+
+//////
+function appendData(auth) {
+    var sheets = google.sheets('v4');
+    sheets.spreadsheets.values.append({
+      auth: auth,
+      spreadsheetId: 'yourSpreadSheetIDHere',
+      range: 'Sheet1!A2:B', //Change Sheet1 if your worksheet's name is something else
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values: [ ["Void", "Canvas", "Website"], ["Paul", "Shan", "Human"] ]
+      }
+    }, (err, response) => {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      } else {
+          console.log("Appended");
+      }
+    });
+  }
+   
+  authentication.authenticate().then((auth)=>{
+      appendData(auth);
+  });
+
+  ///////  
+ 
+function addSheet(auth) {
+    var sheets = google.sheets('v4');
+    sheets.spreadsheets.create({
+      auth: auth,
+      resource: {
+          properties:{
+              title: "Anything-you-name"
+          }
+      }
+    }, (err, response) => {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      } else {
+          console.log("Added");
+      }
+    });
+  }
+   
+  authentication.authenticate().then((auth)=>{
+      addSheet(auth);
+  });
